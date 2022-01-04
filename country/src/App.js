55866160053db2f8f1a9163bd49043dc2928feeb
Dharmesh <yadavdydv@gmail.com>
2022-01-04 15:50:01 +0530

    update

--- country/src/App.js
+++ country/src/App.js
@@ -1,14 +1,30 @@
-
 import "./App.css";
-import { useState } from "react";
-
+import { useState, useCallback } from "react";
+import axios from "axios";
+import debounce from "lodash.debounce";
 function App() {
+  const [newData, setnewDAta] = useState([]);
+  const handleDebounce = useCallback(
+    debounce( async (next) => {
+      setnewDAta([])
+      if(next.length < 2)return false;
+      const {data} = await axios.get(`https://filthy-battledress-tuna.cyclic.app/country/?q=${next}`)
+      setnewDAta(data.country)
+      console.log(data.country);
+    }, 500),
+    []
+  );
   const handleChange = (e) => {
-    console.log(e.target.value);
+    handleDebounce(e.target.value);
   };
   return (
     <div className="App">
-      <input type="text" onChange={(e) => handleChange(e)} />
+      <input
+        type="text"
+        placeholder="Search countries here..."
+        onChange={(e) => handleChange(e)}
+      />
+      <div className="heading">{newData ? newData.map((e) => <p key={e.country}>{e.country}</p>) : ""}</div>
     </div>
   );
 }
